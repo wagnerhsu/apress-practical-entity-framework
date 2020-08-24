b1381e49d513d7359aefc4c102dc91666ebc50f2
Wagner Xu <wagnerhsu@hotmail.com>
2020-08-24 08:56:28 +0800

    Update EF_Activity001

---
+++ ch01/EF_Activity01/CoreFinal/EF_Activity001/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EF_Activity001
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        { 
+        
+        }
+    }
+}
